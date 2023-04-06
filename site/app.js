22bd411d5f2b6ee810426a04be86bcb9da64efe5
Roxana Romaniello <104166649+roxannerbr@users.noreply.github.com>
2023-04-06 20:05:38 -0300

    maquetado
    maquetado listo

---
+++ site/app.js
@@ -0,0 +1,30 @@
+
+/* Entry point */
+let express= require('express')
+let path=require('path')
+let app= express() /* funcion de alto nivel */
+let port =3000 /* puerto */
+
+app.get('/',(req,res)=>res.send("Bienvenidos a De mi para ti"))
+
+/* __dirname nos devuelve la ubicacion del proyecto */
+app.get('/home', (req,res)=>res.sendFile(path.join(__dirname,'views','home.html')))
+app.get('/categorias', (req,res)=>res.sendFile(path.join(__dirname,'views','categorias.html')))
+app.get('/contactanos', (req,res)=>res.sendFile(path.join(__dirname,'views','contactanos.html')))
+app.get('/pregfrec', (req,res)=>res.sendFile(path.join(__dirname,'views','pregfrec.html')))
+app.get('/about', (req,res)=>res.sendFile(path.join(__dirname,'views','about.html')))
+
+/*Requerir Rutas */
+
+let dirname=path.join(__dirname,'views')
+
+app.use(express.static(path.resolve(__dirname,'public')))
+
+/* Levantamos el servidor con app listen */
+app.listen(port,function(){
+  return console.log(`Se levanta el servidor en http://localhost:${port}`)
+});
+
+
+
+
