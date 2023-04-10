5cffc62c4ddeb62a8b9af451d52e55fa27c6441d
Roxana Romaniello <104166649+roxannerbr@users.noreply.github.com>
2023-04-09 22:26:08 -0300

    categorias cargadas

--- site/app.js
+++ site/app.js
@@ -8,11 +8,14 @@ let port =3000 /* puerto */
 app.get('/',(req,res)=>res.send("Bienvenidos a De mi para ti"))
 
 /* __dirname nos devuelve la ubicacion del proyecto */
+app.get('/inicio', (req,res)=>res.sendFile(path.join(__dirname,'views','inicio.html')))
 app.get('/home', (req,res)=>res.sendFile(path.join(__dirname,'views','home.html')))
 app.get('/categorias', (req,res)=>res.sendFile(path.join(__dirname,'views','categorias.html')))
 app.get('/contactanos', (req,res)=>res.sendFile(path.join(__dirname,'views','contactanos.html')))
 app.get('/pregfrec', (req,res)=>res.sendFile(path.join(__dirname,'views','pregfrec.html')))
 app.get('/about', (req,res)=>res.sendFile(path.join(__dirname,'views','about.html')))
+app.get('/novedades', (req,res)=>res.sendFile(path.join(__dirname,'views','novedades.html')))
+
 
 /*Requerir Rutas */
 
